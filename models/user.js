90d31f4dc54faaa311fbd7ad22408a505a6e5f40
Sampan Verma <sampan.developer@gmail.com>
2020-09-24 04:21:12 +0530

    Added timestamps to user

--- models/user.js
+++ models/user.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
-  role: { type: String, required: true, default: "admin" },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+const schema = mongoose.Schema(
+  {
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required: true },
+    role: { type: String, required: true, default: "admin" },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+    },
+    password: { type: String, required: true },
   },
-  password: { type: String, required: true },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("user", schema);
